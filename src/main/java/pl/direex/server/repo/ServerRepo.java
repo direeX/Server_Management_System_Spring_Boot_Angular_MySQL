2ceb2ae7b00ab292b85bca6d4f5f1c585391ade5
direeX <dominik.obara7@gmail.com>
2023-07-23 11:23:21 +0200

    ddd

--- src/main/java/pl/direex/server/repo/ServerRepo.java
+++ src/main/java/pl/direex/server/repo/ServerRepo.java
@@ -5,5 +5,9 @@ import pl.direex.server.model.Server;
 
 public interface ServerRepo extends JpaRepository<Server, Long> {
     // JpaRepository jest interfejsem dostarczanym przez Spring Data JPA, który dostarcza zestaw gotowych metod do wykonywania operacji CRUD (Create, Read, Update, Delete) na encjach (tabelach) w bazie danych. Ten interfejs jest częścią Spring Data JPA, które ułatwia interakcję z bazami danych przy użyciu JPA (Java Persistence API).
-    // <Server, Long> -Server  domena lub klasa którą chcemy zarządzać, Long - typ klucza głównego który podaliśmy w klasie Server
+    // <Server, Long> -Server  domena lub klasa którą chcemy zarządzać, Long - typ klucza głównego który podaliśmy w klasie Server.
+
+    Server findByIpAddress(String ipAddress); // zwróci Server. findBy.. JPA automatycznie sprawdzi co ma wyszukać na podstawie ip.
+
+    Server findByName(String name); // zwróci Server na podstawie name.
 }
